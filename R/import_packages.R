79548ce817f7d9babec04700aa913cf3485a3746
Ashley Sawle <ads2202cu@gmail.com>
2019-10-17 15:54:51 +0100

    Update package imports

--- R/import_packages.R
+++ R/import_packages.R
@@ -1,3 +1,10 @@
-#' @import tidyverse
-#' @import furrr
+#' @import dplyr
+#' @import future
+#' @importFrom tidyr nest unnest
+#' @importFrom purrr map_dbl
+#' @importFrom furrr future_map_dfr
+#' @importFrom stringr str_c
+#' @importFrom readr read_tsv
+#' @importFrom grDevices rainbow
+#' @importFrom stats dist median
 NULL
