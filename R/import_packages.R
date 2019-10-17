b72bab04dedbb7a2fe27427075eb4866ca111c2a
Ashley Sawle <ads2202cu@gmail.com>
2019-10-17 16:22:44 +0100

    Update package imports

--- R/import_packages.R
+++ R/import_packages.R
@@ -1,10 +1,12 @@
 #' @import dplyr
 #' @import future
-#' @importFrom tidyr nest unnest
-#' @importFrom purrr map_dbl
-#' @importFrom furrr future_map_dfr
-#' @importFrom stringr str_c
-#' @importFrom readr read_tsv
+#' @import ggplot2 
 #' @importFrom grDevices rainbow
+#' @importFrom furrr future_map future_map_dfr
+#' @importFrom purrr map_dbl pluck walk
+#' @importFrom readr cols read_tsv write_tsv
 #' @importFrom stats dist median
+#' @importFrom stringr str_c str_detect str_subset
+#' @importFrom tibble rowid_to_column tibble
+#' @importFrom tidyr nest unnest
 NULL
